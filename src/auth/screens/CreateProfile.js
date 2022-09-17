aad035db40228a336db2caa1eb4f0e8adcf4bcdb
Muhammad Usman <106739934+theusman75@users.noreply.github.com>
2022-09-18 00:08:37 +0500

    some minor changes in Create Profile screen

--- src/auth/screens/CreateProfile.js
+++ src/auth/screens/CreateProfile.js
@@ -50,7 +50,7 @@ export default function CreateProfile() {
               
             </View>
 
-            <AntDesign name='plus' size={RFValue(20)} color='#A70032' style={{marginLeft:RFValue(140)}}/>
+            <AntDesign name='plus' size={RFValue(20)} color='#A70032' />
 
           </TouchableOpacity>
 
@@ -62,15 +62,7 @@ export default function CreateProfile() {
           
           <TextInput
             placeholder='Insert your bio here'
-            style={{ 
-              width:'100%', 
-              height:RFValue(48),  
-              borderRadius:8, 
-              paddingHorizontal:RFValue(16), 
-              paddingVertical:RFValue(12), 
-              marginTop:RFValue(8),
-              backgroundColor:'#FAFAFA' 
-            }}
+            style={styles.descriptionInput}
             placeholderTextColor={'#6B7476'}
           />
         
@@ -89,19 +81,19 @@ export default function CreateProfile() {
           <View style={styles.multiContainer}>
             
             <TouchableOpacity style={styles.option}>
-              <Text style={{color:'#373C3E', fontSize:RFValue(14), fontWeight:'400', fontFamily:'Rubik-Regular'}}>2022</Text>
+              <Text style={styles.regularText}>2022</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.option}>
-              <Text style={{color:'#373C3E', fontSize:RFValue(14), fontWeight:'400', fontFamily:'Rubik-Regular'}}>2023</Text>
+              <Text style={styles.regularText}>2023</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.option}>
-              <Text style={{color:'#373C3E', fontSize:RFValue(14), fontWeight:'400', fontFamily:'Rubik-Regular'}}>2024</Text>
+              <Text style={styles.regularText}>2024</Text>
             </TouchableOpacity>
             
             <TouchableOpacity style={styles.option}>
-              <Text style={{color:'#373C3E', fontSize:RFValue(14), fontWeight:'400', fontFamily:'Rubik-Regular'}}>2025</Text>
+              <Text style={styles.regularText}>2025</Text>
             </TouchableOpacity>
 
           </View>
@@ -111,15 +103,15 @@ export default function CreateProfile() {
           <View style={styles.multiContainer}>
             
             <TouchableOpacity style={styles.option}>
-              <Text style={{color:'#373C3E', fontSize:RFValue(14), fontWeight:'400', fontFamily:'Rubik-Regular'}}>Male</Text>
+              <Text style={styles.regularText}>Male</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.option}>
-              <Text style={{color:'#373C3E', fontSize:RFValue(14), fontWeight:'400', fontFamily:'Rubik-Regular'}}>Female</Text>
+              <Text style={styles.regularText}>Female</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.option}>
-              <Text style={{color:'#373C3E', fontSize:RFValue(14), fontWeight:'400', fontFamily:'Rubik-Regular'}}>Non-binary</Text>
+              <Text style={styles.regularText}>Non-binary</Text>
             </TouchableOpacity>
 
           </View>
@@ -132,21 +124,13 @@ export default function CreateProfile() {
 
             </TouchableOpacity>
 
-            <Text style={{color:'#373C3E', fontSize:RFValue(14), fontWeight:'400', fontFamily:'Rubik-Regular'}}>Display gender on profile</Text>
+            <Text style={styles.regularText}>Display gender on profile</Text>
 
           </View>
           
           <Text style={styles.heading}>From</Text>
 
-          <TouchableOpacity style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between', width:'100%', 
-              height:RFValue(48),  
-              borderRadius:8, 
-              paddingHorizontal:RFValue(16), 
-              paddingVertical:RFValue(12), 
-              marginTop:RFValue(8),
-              backgroundColor:'#FAFAFA'
-              }}
-            >
+          <TouchableOpacity style={styles.btnLink}>
             
             <Text style={styles.detail}>Choose where from here</Text>
 
@@ -160,15 +144,7 @@ export default function CreateProfile() {
 
           <Text style={styles.heading}>Instagram</Text>
           
-          <TouchableOpacity style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between', width:'100%', 
-              height:RFValue(48),  
-              borderRadius:8, 
-              paddingHorizontal:RFValue(16), 
-              paddingVertical:RFValue(12), 
-              marginTop:RFValue(8),
-              backgroundColor:'#FAFAFA'
-              }}
-            >
+          <TouchableOpacity style={styles.btnLink}>
             
             <Text style={styles.detail}>Paste your social mdedia link here</Text>
 
@@ -178,15 +154,7 @@ export default function CreateProfile() {
 
           <Text style={styles.heading}>TikTok</Text>
 
-          <TouchableOpacity style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between', width:'100%', 
-              height:RFValue(48),  
-              borderRadius:8, 
-              paddingHorizontal:RFValue(16), 
-              paddingVertical:RFValue(12), 
-              marginTop:RFValue(8),
-              backgroundColor:'#FAFAFA'
-              }}
-            >
+          <TouchableOpacity style={styles.btnLink}>
             
             <Text style={styles.detail}>Paste your social mdedia link here</Text>
 
@@ -196,15 +164,7 @@ export default function CreateProfile() {
           
           <Text style={styles.heading}>Linkedin</Text>
           
-          <TouchableOpacity style={{flexDirection:'row', alignItems:'center', justifyContent:'space-between', width:'100%', 
-              height:RFValue(48),  
-              borderRadius:8, 
-              paddingHorizontal:RFValue(16), 
-              paddingVertical:RFValue(12), 
-              marginTop:RFValue(8),
-              backgroundColor:'#FAFAFA'
-              }}
-            >
+          <TouchableOpacity style={styles.btnLink}>
             
             <Text style={styles.detail}>Paste your social mdedia link here</Text>
 
@@ -214,13 +174,13 @@ export default function CreateProfile() {
 
         <View style={styles.groupHead}/>
 
-        <View style={{width:'100%', flexDirection:'row', alignItems:'center', justifyContent:'space-between', paddingTop:RFValue(30)}}>
+        <View style={styles.intrestContainer}>
 
           <Text style={styles.heading}>Interests</Text>
 
           <TouchableOpacity>
 
-            <Text style={{color:'#A70032', fontSize:RFValue(14), fontWeight:'500', fontFamily:'Rubik-Medium', }}>Choose Interests</Text>
+            <Text style={styles.intrest}>Choose Interests</Text>
 
           </TouchableOpacity>
 
@@ -228,13 +188,13 @@ export default function CreateProfile() {
 
         <View style={styles.groupHead}/>
 
-        <View style={{width:'100%', flexDirection:'row', alignItems:'center', justifyContent:'space-between', paddingVertical:RFValue(30)}}>
+        <View style={styles.intrestContainer}>
 
           <Text style={styles.heading}>Down For</Text>
 
           <TouchableOpacity>
 
-            <Text style={{color:'#A70032', fontSize:RFValue(14), fontWeight:'500', fontFamily:'Rubik-Medium', }}>Choose Down For</Text>
+            <Text style={styles.intrest}>Choose Down For</Text>
 
           </TouchableOpacity>
 
@@ -242,13 +202,13 @@ export default function CreateProfile() {
 
       </ScrollView>
 
-        <View style={{height:RFValue(94), backgroundColor:'#FAFAFA', padding:RFValue(16), width:'100%', alignItems:'center', justifyContent:'center' }}>
-          
-        <TouchableOpacity  style={{backgroundColor:'#E3E8EB', width:'100%', height:RFValue(42),  alignItems:'center', justifyContent:'center', borderRadius:8}}>
-          <Text style={{color:'#B9BFC1', fontSize:RFValue(14), fontWeight:'600', fontFamily:'Rubik-Medium', }}>Confirm Change</Text>
+      <View style={styles.buttonContainer}>
+        
+        <TouchableOpacity  style={styles.btn}>
+          <Text style={styles.btnTitle}>Confirm Change</Text>
         </TouchableOpacity>
 
-        </View>
+      </View>
 
 
     </View>
@@ -329,18 +289,80 @@ const styles = StyleSheet.create({
     paddingVertical:RFValue(12), 
     marginTop:RFValue(8),
     backgroundColor:'#FAFAFA'
-    },
-    multiContainer: {
-      flexDirection:'row', 
-      alignItems:'center', 
-      marginTop:RFValue(15)
-    },
-    option: {
-      borderWidth:0.5, 
-      borderRadius:5, 
-      borderColor:'gray',
-      marginRight:RFValue(15), 
-      paddingVertical:RFValue(3), 
-      paddingHorizontal:RFValue(5)
-    }
+  },
+  multiContainer: {
+    flexDirection:'row', 
+    alignItems:'center', 
+    marginTop:RFValue(15)
+  },
+  option: {
+    borderWidth:0.5, 
+    borderRadius:5, 
+    borderColor:'gray',
+    marginRight:RFValue(15), 
+    paddingVertical:RFValue(3), 
+    paddingHorizontal:RFValue(5)
+  },
+  regularText: {
+    color:'#373C3E', 
+    fontSize:RFValue(14), 
+    fontWeight:'400', 
+    fontFamily:'Rubik-Regular'
+  },
+  intrestContainer: {
+    width:'100%', 
+    flexDirection:'row', 
+    alignItems:'center', 
+    justifyContent:'space-between', 
+    paddingTop:RFValue(30)
+  },
+  intrest: {
+    color:'#A70032', 
+    fontSize:RFValue(14), 
+    fontWeight:'500', 
+    fontFamily:'Rubik-Medium', 
+  },
+  buttonContainer: {
+    height:RFValue(94), 
+    backgroundColor:'#FAFAFA', 
+    padding:RFValue(16), 
+    width:'100%', 
+    alignItems:'center', 
+    justifyContent:'center' 
+  },
+  btn: {
+    backgroundColor:'#E3E8EB', 
+    width:'100%', 
+    height:RFValue(42), 
+    alignItems:'center', 
+    justifyContent:'center', 
+    borderRadius:8
+  },
+  btnTitle: {
+    color:'#B9BFC1', 
+    fontSize:RFValue(14), 
+    fontWeight:'600', 
+    fontFamily:'Rubik-Medium',
+  },
+  btnLink: {
+    flexDirection:'row', 
+    alignItems:'center', 
+    justifyContent:'space-between', 
+    width:'100%', 
+    height:RFValue(48),  
+    borderRadius:8, 
+    paddingHorizontal:RFValue(16), 
+    paddingVertical:RFValue(12), 
+    marginTop:RFValue(8),
+    backgroundColor:'#FAFAFA'
+  },
+  descriptionInput: { 
+    width:'100%', 
+    height:RFValue(48),  
+    borderRadius:8, 
+    paddingHorizontal:RFValue(16), 
+    paddingVertical:RFValue(12), 
+    marginTop:RFValue(8),
+    backgroundColor:'#FAFAFA' 
+  }
 })
